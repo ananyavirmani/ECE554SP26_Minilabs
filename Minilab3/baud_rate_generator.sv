f3763996e03affd6ffbbe1e4c2278d1dcdbaaffb
Harshul Jalan <harshuljalan@Harshuls-MacBook-Pro-14.local>
2026-02-24 16:25:55 -0600

    BRG

--- Minilab3/baud_rate_generator.sv
+++ Minilab3/baud_rate_generator.sv
@@ -1,33 +1,37 @@
 module baud_rate_generator(
     input clk,
     input rst,
+    input wr_low,
+    input wr_high,
     input [7:0] db_high,
     input [7:0] db_low,
     output logic baud_en
 );
 
 logic [15:0] divisor;
-
-logic load;
-
-assign load = (divisor == 16'h0000);
+logic [15:0] store;
 
 always_ff @(posedge clk or posedge rst) begin
-    if (rst)
-        divisor <= 16'h0145;
-    else if (load)
-        divisor <= {db_high, db_low};
-    else
-        divisor <= divisor - 1'b1;
-end
-
-always_ff @(posedge clk or posedge rst) begin
-    if (rst)
+    if (rst) begin
+        divisor <= 16'h0146;
         baud_en <= 1'b0;
-    else if (divisor == 16'h0000)
+
+    end else if (divisor == 16'h0000) begin
+        divisor <= store;
         baud_en <= 1'b1;
-    else
+
+    end else begin
         baud_en <= 1'b0;
+        if (wr_low)
+            store[7:0] <= db_low;
+
+        else if (wr_high)
+            store[15:8] <= db_high;
+
+        else
+            divisor <= divisor - 1'b1;
+    end 
+        
 end
 
 
